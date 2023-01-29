316d3c8c1a3f2a579059d8286d9a4c1369a22cdd
Nishmitha <nishmithasullal@gmail.com>
2023-01-29 22:16:54 +0530

    Changes

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -6,11 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {}
+
   public alphabets: string[][] = [["A", "B", "C", "D", "E"],
                                 ["F", "G", "H", "I", "J"],
                                 ["K", "L", "M", "N", "O"],
                                 ["P", "Q", "R", "S", "T"],
                                 ["U", "V", "W", "X", "Y"],
                                 ["Z"]];
+  public indexY = 0;
+  public answerX = 0;
+  public userSelectedOptions: string[][] = [[],[],[],[],[]];
+  public name:string = "";
 
+  public onSelectFirst(value: string[]): void {
+    for(const key in value) {
+      this.userSelectedOptions[key][this.indexY]= value[key];
+    }
+    this.indexY++;
+  }
+
+  public onSubmit(data: string): void {
+    let numberOfLetters = Number(data);
+    if( numberOfLetters < 1 && numberOfLetters > 15)
+      numberOfLetters = 0;
+  }
+  
+  public onSelectSecond(index: number): void {
+    this.name = this.name + this.userSelectedOptions[index][this.answerX];
+    this.answerX++;
+    console.log(this.name);
+  }
 }
